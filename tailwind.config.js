3a65e5394505c81cabf31f0602fa0d8b540fa835
shaharafat <shaharafhat@gmail.com>
2021-05-01 05:50:27 +0600

    custom document, color and font added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        purpleDark: '#3C2F5E'
+      },
+      fontFamily: {
+        merriweather: ['Merriweather', 'sans-serif'],
+        openSans: ['Open Sans', 'serif'],
+      },
+      colors: {
+        darkBlue: '#7178BC',
+      }
+    },
   },
   variants: {
     extend: {},
