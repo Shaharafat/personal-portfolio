408268172f0982748f589169ec93f1852135d5e3
shaharafat <shaharafhat@gmail.com>
2021-05-01 15:52:03 +0600

    dark mode added

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,22 @@
-import Head from 'next/head'
+import { useTheme } from "next-themes";
+import Head from "next/head";
 
 export default function Home() {
+  const { theme, setTheme } = useTheme();
   return (
-    <div >
+    <div>
       <Head>
         <title>Portfolio | Shah Arafat</title>
         <meta name="description" content="Portfolio website of Shah Arafat" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className=" grid place-items-center min-h-screen font-merriweather text-4xl font-bold bg-purpleDark text-darkBlue">
+      <main className=" grid place-items-center min-h-screen font-merriweather text-4xl font-bold bg-white dark:bg-purpleDark text-darkBlue">
         Hello
+        <button onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
+          Toggle
+        </button>
       </main>
-
     </div>
-  )
+  );
 }
